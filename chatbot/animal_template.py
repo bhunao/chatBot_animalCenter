16683a0ecb4507ea875c83953b455b0dd1f129bf
felix <bruno.felix@e-deploy.com.br>
2022-05-26 12:57:50 -0300

    lower

--- chatbot/animal_template.py
+++ chatbot/animal_template.py
@@ -9,15 +9,14 @@ class Animal:
         self.options = ["gato", "cachorro"]
 
     def anwser(self, text:str) -> str:
-        print(f"options are: {self.options}")
-        print(f"person said: {text}")
         selected_option = text.lower()
+        print(f"options are: {self.options}")
+        print(f"person said: {selected_option}")
         #if selected_option in self.dialogo.keys():
         if selected_option in self.options:
                 self.step = selected_option
         else:
-            self.msg = "Opção invalida!"
-            return self.msg
+            return "Opção invalida!"
         
         self.msg = self.dialogo[selected_option]["msg"]
         self.options = self.dialogo[selected_option]["options"]
